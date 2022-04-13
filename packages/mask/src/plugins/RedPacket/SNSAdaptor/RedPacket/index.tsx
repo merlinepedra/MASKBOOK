0fe0a2be18d63e6cb21895faf7e0d44d58970fc4
seiko <seiko_des@outlook.com>
2022-04-13 15:25:28 +0800

    feat: rety redpacket if native claim or refund

--- packages/mask/src/plugins/RedPacket/SNSAdaptor/RedPacket/index.tsx
+++ packages/mask/src/plugins/RedPacket/SNSAdaptor/RedPacket/index.tsx
@@ -138,7 +138,7 @@ export function RedPacket(props: RedPacketProps) {
 
     const onClaimOrRefundOnNative = useCallback(async () => {
         if (!availability) return
-        await nativeAPI?.api.claimOrRefundRedpacket({
+        const success = await nativeAPI?.api.claimOrRefundRedpacket({
             redpacketPayload: payload,
             availability: {
                 token_address: availability.token_address,
@@ -150,6 +150,9 @@ export function RedPacket(props: RedPacketProps) {
             },
             postLink: postLink.toString(),
         })
+        if (success) {
+            revalidateAvailability()
+        }
     }, [availability, JSON.stringify(payload), postLink])
 
     const myStatus = useMemo(() => {
